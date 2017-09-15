ad7bcd54b8ffb0166c63348f739cd0dbe95599a1
lanbo.gao <lanbo.gao@gmail.com>
2017-09-15 20:59:45 +0800

    use tun intf id to change route table

--- bins/client_up.bat
+++ bins/client_up.bat
@@ -15,26 +15,27 @@ REM SET orig_intf="WLAN 2"
  
 REM exclude remote server in routing table
 for /F "tokens=3" %%* in ('route print ^| findstr "\<0.0.0.0\>"') do set "orig_gw=%%*"
-route add %server% %orig_gw% metric 5 > NUL
- 
+route change 0.0.0.0/0 %orig_gw%
+route add %server% %orig_gw%
+
 REM configure IP address and MTU of VPN interface
 REM netsh interface ip set interface %orig_intf% ignoredefaultroutes=enabled > NUL
-REM netsh interface ip set address name="%intf%" static %tunip% 255.255.255.0 > NUL
-netsh interface ip set address name="%intf%" static %tunip% 255.255.255.0 %remote_tun_ip% gwmetric=10000 > NUL
+netsh interface ip set address name="%intf%" static %tunip% 255.255.255.0 > NUL
+REM netsh interface ipv4 set address name="%intf%" static %tunip% 255.255.255.0 %remote_tun_ip% gwmetric=10000 > NUL
 REM netsh interface ip set interface "%intf%" metric=1 > NUL
 netsh interface ipv4 set subinterface "%intf%" mtu=%mtu% > NUL
  
 REM change routing table
 ECHO changing default route
 REM checking if winxp
-ver | find "5.1" > NUL
-if %ERRORLEVEL%==0 (
-    route add 128.0.0.0 mask 128.0.0.0 %remote_tun_ip% metric 1 > NUL
-    route add 0.0.0.0 mask 128.0.0.0 %remote_tun_ip% metric 1 > NUL
-) else (
-    netsh interface ipv4 add route 128.0.0.0/1 "%intf%" %remote_tun_ip% metric=0 > NUL
-    netsh interface ipv4 add route 0.0.0.0/1 "%intf%" %remote_tun_ip% metric=0 > NUL
-)
+REM ver | find "5.1" > NUL
+REM if %ERRORLEVEL%==0 (
+    route add 128.0.0.0 mask 128.0.0.0 %remote_tun_ip% metric 1 IF %intf_id% > NUL
+    route add 0.0.0.0 mask 128.0.0.0 %remote_tun_ip% metric 1  IF %intf_id% > NUL
+REM ) else (
+REM     netsh interface ipv4 add route 128.0.0.0/1 "%intf%" %remote_tun_ip% metric=0 > NUL
+REM     netsh interface ipv4 add route 0.0.0.0/1 "%intf%" %remote_tun_ip% metric=0 > NUL
+REM )
 ECHO default route changed to %remote_tun_ip%
  
 REM change dns server
